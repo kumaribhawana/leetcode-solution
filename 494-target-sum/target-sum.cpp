92afaab1a806fea7ce089e8f4d7930d683edaa94
kumaribhawana <bhawanak291@gmail.com>
2026-08-02 15:19:21 +0530

    Time: 775 ms (16.75%) | Memory: 11.6 MB (96.66%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int ans  = 0;
+    void solve(vector<int>& nums, int target, int curr, int idx) {
+            if (idx == nums.size()) {
+            if (curr == target) {
+                ans++;
+            }
+            return;
+        }
+         solve(nums, target, curr + nums[idx], idx + 1);
+         solve(nums, target, curr - nums[idx], idx + 1);
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        //ans = 0;
+        solve(nums, target, 0, 0);
+        return ans;
+    }
+};
