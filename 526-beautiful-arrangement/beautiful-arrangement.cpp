ae2e93f4ddc7dae742a00c00c5995f30acf84240
kumaribhawana <bhawanak291@gmail.com>
2026-08-03 11:24:37 +0530

    Time: 45 ms (51.90%) | Memory: 7.9 MB (83.60%) - LeetSync

---
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void calculate(int n, int pos, vector<bool>& visited, int &count) {
+         if (pos > n) {
+            count++;
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
+                visited[i] = true;
+                calculate(n, pos + 1, visited, count);
+                visited[i] = false; 
+            }
+        }
+    }
+
+    int countArrangement(int n) {
+        vector<bool> visited(n + 1, false);
+        int count = 0;
+        calculate(n, 1, visited, count);
+        return count;
+    }
+};
