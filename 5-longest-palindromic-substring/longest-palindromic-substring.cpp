5f858635fe0a1344143dec658adde995f50c20cf
kumaribhawana <bhawanak291@gmail.com>
2026-08-29 11:10:56 +0530

    Time: 847 ms (9.14%) | Memory: 13.8 MB (39.48%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    bool solve(string &s, int i, int j) {
-        if (i >= j)
+    bool solve(string& s, int i, int j) {
+
+        if (i >= j) {
             return true;
-
-        if (s[i] == s[j])
+        }
+        if (s[i] == s[j]) {
             return solve(s, i + 1, j - 1);
-
+        }
         return false;
     }
-
     string longestPalindrome(string s) {
+
         int n = s.length();
-
-        int maxlen = 1;
-        int sp = 0;
-
+        string ans = "";
+        int maxl = 0;
+        int curren = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                if (solve(s, i, j)) {
-                    if (j - i + 1 > maxlen) {
-                        maxlen = j - i + 1;
-                        sp = i;
+                if (solve(s, i, j) == true) {
+                    if (j - i + 1 > maxl) {
+                        maxl = j - i + 1;
+                        ans = s.substr(i, j - i + 1);
                     }
+                    curren = i;
                 }
             }
         }
-
-        return s.substr(sp, maxlen);
+        return ans;
     }
 };
