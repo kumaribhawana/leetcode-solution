82fc515f572f8b5c12fb1e6c9004b56147743251
kumaribhawana <bhawanak291@gmail.com>
2026-08-30 10:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (89.02%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,29 @@
+ class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.length();
+        stack<char> st;
+             for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+                if (ch == ')' && st.top() != '(') {
+                    return false;
+                }
+                 if (ch == '}' && st.top() != '{') {
+                    return false;
+                }
+                 if (ch == ']' && st.top() != '[') {
+                    return false;
+                }
+               st.pop();
+            }
+        }
+          return st.empty();
+    }
+};
