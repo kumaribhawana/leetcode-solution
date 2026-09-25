c37b1f51b7ee2d1b14a64652c6039f7b57f97afc
kumaribhawana <bhawanak291@gmail.com>
2026-09-25 11:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (45.90%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,18 +11,17 @@
  */
 class Solution {
 public:
-void solve(TreeNode*root,vector<int>&ans){
-    if(root==NULL){
+   void solve(TreeNode* root,vector<int>& ans){
+      if(root==NULL){
         return;
-    }
-    solve(root->left,ans);
-    solve(root->right,ans);
-    ans.push_back(root->val);
-    
-}
+      }
+      solve(root->left,ans);
+      solve(root->right,ans);
+      ans.push_back(root->val);
+   }
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        solve (root,ans);
-        return ans;
+         vector<int>ans;
+         solve(root,ans);
+         return ans;
     }
 };
